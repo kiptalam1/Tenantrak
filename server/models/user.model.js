180c58d8ac1a93fa6ea687f2d96bd86dd67fb995
kiptalam1 <adamskiptalam0@gmail.com>
2025-08-07 13:17:52 +0300

    feat: create a basic user model.

---
+++ server/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: [true, "Provide your full names."],
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: [true, "Email is required."],
+			unique: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: [true, "Password is required."],
+			trim: true,
+			minlength: [6, "Password must be at least 6 characters long"],
+		},
+		role: {
+			type: String,
+			enum: ["owner", "caretaker", "tenant"],
+			default: "owner",
+		},
+	},
+	{ timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;

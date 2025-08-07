fe9a4b201cdb504ff797987c6b9ff241245ec44c
kiptalam1 <adamskiptalam0@gmail.com>
2025-08-07 16:20:12 +0300

    add function to verify tokens

---
+++ server/middlewares/verifyToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const verifyToken = (req, res, next) => {
+	const token = res.cookies?.token;
+	if (!token)
+		return res.status(403).json({ error: "Access denied. Please login" });
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
+	} catch (error) {
+		return res.status(401).json({ error: "Invalid or expired token." });
+	}
+};
+
+export default verifyToken;

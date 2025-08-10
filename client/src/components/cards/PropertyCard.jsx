0f61b055054a41050bf359e7c00087c6877388e8
kiptalam1 <adamskiptalam0@gmail.com>
2025-08-10 14:39:23 +0300

    feat: display room data from api.

--- client/src/components/cards/PropertyCard.jsx
+++ client/src/components/cards/PropertyCard.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-import { Bath, Bed, Trash2, Edit } from "lucide-react";
+import { Bath, Bed, Trash2, Edit, House } from "lucide-react";
 
-const PropertyCard = ({ name, status }) => {
+const PropertyCard = ({
+	buildingName,
+	roomName,
+	status,
+	bath,
+	bed,
+	price,
+	roomType,
+	tenant,
+}) => {
 	const statusColorMap = {
 		occupied: "bg-green-200 text-green-800",
 		vacant: "bg-yellow-200 text-yellow-800",
@@ -10,23 +18,29 @@ const PropertyCard = ({ name, status }) => {
 	return (
 		<div className="text-sm bg-gray-50 dark:bg-neutral-900 text-gray-900 dark:text-gray-100 flex flex-col justify-between gap-3 w-full sm:w-64 md:w-72 max-w-md border border-gray-300 dark:border-gray-700 rounded-xl p-5 shadow-sm">
 			<div className="flex items-center justify-between">
-				<p>{name}</p>
+				<p className="font-bold italic">
+					{buildingName} {roomName}
+				</p>
 				<div className={`${statusColorMap[status]} py-0.5 px-2 rounded-lg`}>
 					{status}
 				</div>
 			</div>
+			<p className="flex items-center gap-1 ">
+				<House size={16} />
+				{roomType}
+			</p>
 			<div className="flex items-center gap-4">
 				<p className="flex items-center gap-1 ">
-					<Bed size={16} /> 2 bed
+					<Bed size={16} /> {bed} bed
 				</p>
 				<p className="flex items-center gap-1 ">
-					<Bath size={16} /> 1 bath
+					<Bath size={16} /> {bath} bath
 				</p>
 			</div>
 			<div className="flex items-center justify-between gap-3">
 				<div>
-					<p className="font-bold">Ksh 7,000/month</p>
-					<p className="text-xs">Tenant: John Doe</p>
+					<p className="font-bold">Ksh {price}/month</p>
+					<p className="text-xs">Tenant: {tenant}</p>
 				</div>
 				<div className="flex items-center justify-between gap-3">
 					<Edit
--- client/src/components/containers/PropertiesContainer.jsx
+++ client/src/components/containers/PropertiesContainer.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+import toast from "react-hot-toast";
 import PropertyCard from "../cards/PropertyCard";
 import Button from "../common/Button";
 import { Plus } from "lucide-react";
@@ -6,6 +7,26 @@ import AddPropertyModal from "../AddPropertyModal";
 
 const PropertiesContainer = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const [rooms, setRooms] = useState([]);
+
+	// fetch all properties;
+	useEffect(() => {
+		const fetchProperties = async () => {
+			try {
+				const res = await fetch("/api/rooms/all", { credentials: "include" });
+				const data = await res.json();
+				// console.dir(data);
+				if (!res.ok)
+					return toast.error(data?.error || "Failed to load properties");
+				setRooms(data.rooms);
+			} catch (error) {
+				console.error(error);
+				toast.error("Something went wrong");
+			}
+		};
+		fetchProperties();
+	}, []);
 
 	const openModal = () => setIsModalOpen(true);
 	const closeModal = () => setIsModalOpen(false);
@@ -22,10 +43,20 @@ const PropertiesContainer = () => {
 				/>
 			</div>
 			<div className="flex items-center flex-wrap gap-4">
-				<PropertyCard name={"001"} status={"occupied"} />
-				<PropertyCard name={"002"} status={"vacant"} />
-				<PropertyCard name={"003"} status={"maintenance"} />
-				<PropertyCard name={"006"} status={"occupied"} />
+				{rooms &&
+					rooms.map((room) => (
+						<PropertyCard
+							key={room._id}
+							buildingName={room.buildingName}
+							roomName={room.roomName}
+							status={room.status}
+							roomType={room.roomType}
+							bath={room.bath}
+							bed={room.bed}
+							price={room.price}
+							tenant={room.tenants[0]}
+						/>
+					))}
 			</div>
 		</div>
 	);
